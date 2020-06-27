ee773f1a954a4856dce681984ec40c89905db024
Qutubuddin <qutubuddiniam@gmail.com>
2020-06-27 17:46:15 +0530

    Updates

---
+++ precache-manifest.2208e6a9917b465001db674af3e0d9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48eab5cf234e5002ab939220478f1c10",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5c9aefa6b78445bde768",
+    "url": "/todolist/static/css/main.597729c2.chunk.css"
+  },
+  {
+    "revision": "0c248514d5c0707455a4",
+    "url": "/todolist/static/js/2.b744b007.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.b744b007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9aefa6b78445bde768",
+    "url": "/todolist/static/js/main.085c3fb7.chunk.js"
+  },
+  {
+    "revision": "94e2713e27bcde79676a",
+    "url": "/todolist/static/js/runtime-main.86e7f888.js"
+  }
+]);
